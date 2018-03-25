2105f3346e5ebef40a4ed34b532e70b7a34ff1b0
yogesh <yogeshlcse@hotmail.com>
2018-03-24 21:54:15 -0400

    Book

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,31 +1,52 @@
-import {Component} from "@angular/core";
-import {Book} from "../shared/book";
+import { Component } from "@angular/core";
+import { Book } from "../shared/book";
+import { ActivatedRoute, Router } from "@angular/router";
+import { GoogleBooksService } from "../shared/google-books.service";
+import { LibraryService } from "../shared/library.service";
 
 @Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  selector: "app-book",
+  templateUrl: "./book.component.html",
+  styleUrls: ["./book.component.css"]
 })
 export class BookComponent {
+  private book: Book;
 
-
-  constructor() {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private googleBooksService: GoogleBooksService,
+    private libraryService: LibraryService
+  ) {
+    this.route.params.subscribe(params => {
+      if (params["bookId"]) {
+        this.getBook(params["bookId"]);
+      }
+    });
   }
 
   getBook(bookId: string) {
-    //TODO
+    this.googleBooksService
+      .retrieveBook(bookId)
+      .do(value => console.log(value))
+      .subscribe(value => (this.book = value));
   }
 
   hasBook(book: Book): boolean {
-    //TODO
-    return false;
+    if (book) {
+      return this.libraryService.hasBook(book);
+    }
   }
 
   addBook(book: Book) {
-    //TODO
+    if (book) {
+      return this.libraryService.addBook(book);
+    }
   }
 
   removeBook(book: Book) {
-    //TODO
+    if (book) {
+      return this.libraryService.removeBook(book);
+    }
   }
 }
